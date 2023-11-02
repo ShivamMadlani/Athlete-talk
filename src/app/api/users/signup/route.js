68a1dc5f55651a857bb90e3a0781ea950617738a
Shivam <shivammadlani5@gmail.com>
2023-11-03 01:44:53 +0530

    pushed data to db(signin)

--- src/app/api/users/signup/route.js
+++ src/app/api/users/signup/route.js
@@ -1,35 +1,19 @@
-const nc = require('next-connect');
 const dbConnect = require('../../../../db/mongoose');
 const User = require('../../../../db/models/userModel');
 const authController = require('../../../../authController');
-// const AppError = require('./../../../../utils/appError');
-const catchAsync = require('../../../../utils/catchAsync');
+import { NextResponse } from 'next/server';
 
-console.log('log')
+await dbConnect();
 
-export async function POST(req) {
-
+export async function POST(req, res) {
+    const data = await req.json();
+    const newUser = await User.create({
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        passwordConfirm: data.passwordConfirm,
+        role: data.role,
+    });
+    authController.createSendToken(newUser, 201, res);
+    return NextResponse.json(data);
 }
-
-// const handler = nc({
-//     onError: authController.handleError,
-//     onNoMatch: authController.handleNoMatch,
-// });
-
-// handler.post(
-//     catchAsync(async (req, res, next) => {
-//         await dbConnect();
-
-//         const newUser = await User.create({
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: req.body.password,
-//             passwordConfirm: req.body.passwordConfirm,
-//             role: req.body.role,
-//         });
-
-//         authController.createSendToken(newUser, 201, res);
-//     })
-// );
-
-// export default handler;
