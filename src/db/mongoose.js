47c6ccda17d814a6e34e8b6b4141ec641b23252d
Shivam <shivammadlani5@gmail.com>
2023-11-03 01:00:41 +0530

    adding auth

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,44 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectDb = () => {
-    mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
-        console.log("MongoDB connected");
-    }).catch(err => console.error(err));
+if (!MONGODB_URI) {
+    throw new Error(
+        'Please define the MONGODB_URI environment variable inside .env.local'
+    );
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            console.log('Connected to MongoDB');
+            require('./models/userModel');
+            // require('./../models/categoryModel');
+            // require('./../models/videoModel');
+            // require('./../models/planModel');
+            // require('./../models/messageModel');
+            // require('./../models/chatModel');
+            // require('./../models/userPlanModel');
+            return mongoose;
+        });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+module.exports = dbConnect;
