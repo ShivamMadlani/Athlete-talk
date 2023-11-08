f5a96c83ee6f97ee08bbfd94c48490a5c95d3e58
Shivam <shivammadlani5@gmail.com>
2023-11-08 18:17:21 +0530

    added create plan route

--- src/app/api/dashboard/route.js
+++ src/app/api/dashboard/route.js
@@ -45,6 +45,5 @@ export async function GET(req) {
                 status: flag ? error.statusCode : 500
             },
         );
-
     }
 }
--- src/app/api/makeplan/route.js
+++ src/app/api/makeplan/route.js
@@ -1,26 +1,62 @@
 const dbConnect = require('../../../../db/mongoose');
 import Plan from '../../../db/models/planModel';
 import { NextResponse } from 'next/server';
+const authController = require('../../../authController');
+
+export async function GET(req) {
+    try {
+        await authController.protect(req);
+
+        const plans = await Plan.find({}).populate('creator');
+
+        return NextResponse.json(
+            {
+                status: 'success',
+                results: plans.length,
+                data: {
+                    plans,
+                    preferredCategories: req.user.preferredCategories,
+                },
+            });
+    } catch (error) {
+        console.log(error);
+        const flag = error instanceof AppError
+        return NextResponse.json(
+            {
+                error: flag ? error.message : 'Internal Server Error'
+            },
+            {
+                status: flag ? error.statusCode : 500
+            },
+        );
+    }
+}
 
 export async function POST(req) {
-    await dbConnect();
-    const data = await req.json();
-    const newPlan = await Plan.create({
-        name: data.name,
-        description: data.description,
-        noOfDays: data.noOfDays,
-        videos: data.videos,
-        categories: data.categories,
-        createdAt: data.createdAt,
-        creator: data.creator,
-        duration: data.duration
-    });
-    return NextResponse.json(
-        {
-            message : "New Plan made"
-        },
-        {
-            status : 200
-        }
-    );
+    try {
+        await authController.protect(req);
+
+        const plan = Object.assign(req.body, {
+            creator: req.user._id.toString(),
+        });
+
+        await Plan.create(plan);
+
+        return NextResponse.json(
+            {
+                status: 'success',
+                message: 'Plan Added Successfully',
+            });
+    } catch (error) {
+        console.log(error);
+        const flag = error instanceof AppError
+        return NextResponse.json(
+            {
+                error: flag ? error.message : 'Internal Server Error'
+            },
+            {
+                status: flag ? error.statusCode : 500
+            },
+        );
+    }
 }
