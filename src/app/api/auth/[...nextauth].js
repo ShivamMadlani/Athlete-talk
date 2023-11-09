650fd22c6a8136e8dfffa0032f0dc21ca9568cd2
SHREYANSHVYAS0001 <shreyanshvyas9999@gmail.com>
2023-11-09 18:21:31 +0530

    creating folder to authenticate google sign in  using next-auth

---
+++ src/app/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import User from "../../../db/models/user.js";
+import bcrypt from "bcryptjs";
+import dbConnect from "../../../config/dbConnect.js";
+import GoogleProvider from "next-auth/providers/google";
+export default NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        dbConnect();
+
+        const { email, password } = credentials;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new Error("Invalid Email or Password");
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatched) {
+          throw new Error("Invalid Email or Password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
