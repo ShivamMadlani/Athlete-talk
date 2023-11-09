7a6904d8452a4be4149d3f6fa7a42361115dac70
Mauli <mauli.bhavsar24@gmail.com>
2023-11-10 01:06:29 +0530

    profile page added

--- components/MyProfile.js
+++ components/MyProfile.js
@@ -1,51 +1,43 @@
-"use client";
 import React from "react";
-import AspectRatio from "@mui/joy/AspectRatio";
-import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import Divider from "@mui/joy/Divider";
-import FormControl from "@mui/joy/FormControl";
-import FormLabel from "@mui/joy/FormLabel";
-import FormHelperText from "@mui/joy/FormHelperText";
-import Input from "@mui/joy/Input";
-import IconButton from "@mui/joy/IconButton";
-import Textarea from "@mui/joy/Textarea";
-import Stack from "@mui/joy/Stack";
-import Select from "@mui/joy/Select";
-import Option from "@mui/joy/Option";
-import Typography from "@mui/joy/Typography";
-import Tabs from "@mui/joy/Tabs";
-import TabList from "@mui/joy/TabList";
-import Tab, { tabClasses } from "@mui/joy/Tab";
+import {
+  Box,
+  Typography,
+  Tabs,
+  TabList,
+  Tab,
+  tabClasses,
+  Divider,
+  Stack,
+  IconButton,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Option,
+  CardOverflow,
+  CardActions,
+  Button,
+} from "@mui/joy";
 import Breadcrumbs from "@mui/joy/Breadcrumbs";
 import Link from "@mui/joy/Link";
 import Card from "@mui/joy/Card";
-import CardActions from "@mui/joy/CardActions";
-import CardOverflow from "@mui/joy/CardOverflow";
-import SvgIcon from "@mui/joy/SvgIcon";
-import { styled } from "@mui/joy";
-
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Textarea from "@mui/joy/Textarea";
+import FormHelperText from "@mui/joy/FormHelperText";
 import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
 import VideocamRoundedIcon from "@mui/icons-material/VideocamRounded";
 import InsertDriveFileRoundedIcon from "@mui/icons-material/InsertDriveFileRounded";
-import EditRoundedIcon from "@mui/icons-material/EditRounded";
+
+// import DropZone from "./DropZone";
+// import FileUpload from "./FileUpload";
+// import CountrySelector from "./CountrySelector";
+// import EditorToolbar from "./EditorToolbar";
 
 export default function MyProfile() {
-  const VisuallyHiddenInput = styled("input")`
-    clip: rect(0 0 0 0);
-    clip-path: inset(50%);
-    height: 1px;
-    overflow: hidden;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    white-space: nowrap;
-    width: 1px;
-  `;
-
   return (
     <Box
       sx={{
@@ -72,30 +64,108 @@ export default function MyProfile() {
             },
           }}
         >
-          <Breadcrumbs
-            size="sm"
-            aria-label="breadcrumbs"
-            separator={<ChevronRightRoundedIcon fontSize="sm" />}
-            sx={{ pl: 0 }}
+          <Stack
+            spacing={2}
+            sx={{
+              mt: 1,
+              mb: 2,
+            }}
           >
-            <Link
-              underline="none"
-              color="neutral"
-              href="#some-link"
-              aria-label="Home"
+            <Breadcrumbs
+              size="sm"
+              aria-label="breadcrumbs"
+              separator={<ChevronRightRoundedIcon fontSize="sm" />}
+              sx={{ pl: 0 }}
             >
-              <HomeRoundedIcon />
-            </Link>
-            <Link
-              underline="hover"
-              color="neutral"
-              href="#some-link"
-              fontSize={12}
-              fontWeight={500}
+              <Link
+                underline="none"
+                color="neutral"
+                href="#some-link"
+                aria-label="Home"
+              >
+                <HomeRoundedIcon />
+              </Link>
+              <Link
+                underline="hover"
+                color="neutral"
+                href="#some-link"
+                fontSize={12}
+                fontWeight={500}
+              >
+                Users
+              </Link>
+              <Typography color="primary" fontWeight={500} fontSize={12}>
+                My profile
+              </Typography>
+            </Breadcrumbs>
+            <Typography
+              level="h2"
+              sx={{
+                mt: 1,
+                mb: 2,
+              }}
             >
-              Users
-            </Link>
-          </Breadcrumbs>
+              My profile
+            </Typography>
+          </Stack>
+          <Tabs
+            defaultValue={0}
+            sx={{
+              bgcolor: "transparent",
+            }}
+          >
+            <TabList
+              tabFlex={1}
+              size="sm"
+              sx={{
+                pl: {
+                  xs: 0,
+                  md: 4,
+                },
+                justifyContent: "left",
+                [`&& .${tabClasses.root}`]: {
+                  flex: "initial",
+                  bgcolor: "transparent",
+                  [`&.${tabClasses.selected}`]: {
+                    fontWeight: "600",
+                    "&::after": {
+                      height: "2px",
+                      bgcolor: "primary.500",
+                    },
+                  },
+                },
+              }}
+            >
+              <Tab
+                sx={{ borderRadius: "6px 6px 0 0" }}
+                indicatorInset
+                value={0}
+              >
+                Settings
+              </Tab>
+              <Tab
+                sx={{ borderRadius: "6px 6px 0 0" }}
+                indicatorInset
+                value={1}
+              >
+                Team
+              </Tab>
+              <Tab
+                sx={{ borderRadius: "6px 6px 0 0" }}
+                indicatorInset
+                value={2}
+              >
+                Plan
+              </Tab>
+              <Tab
+                sx={{ borderRadius: "6px 6px 0 0" }}
+                indicatorInset
+                value={3}
+              >
+                Billing
+              </Tab>
+            </TabList>
+          </Tabs>
         </Box>
       </Box>
 
@@ -117,10 +187,9 @@ export default function MyProfile() {
       >
         <Card>
           <Box sx={{ mb: 1 }}>
-            <Typography level="title-md">Upload Video</Typography>
+            <Typography level="title-md">Personal info</Typography>
             <Typography level="body-sm">
-              Select the .mp4 file that you want to upload and enter appropriate
-              description and category of the video.
+              Customize how your profile information will apper to the networks.
             </Typography>
           </Box>
           <Divider />
@@ -129,8 +198,40 @@ export default function MyProfile() {
             spacing={3}
             sx={{ display: { xs: "none", md: "flex" }, my: 1 }}
           >
+            <Stack direction="column" spacing={1}>
+              <AspectRatio
+                ratio="1"
+                maxHeight={200}
+                sx={{ flex: 1, minWidth: 120, borderRadius: "100%" }}
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
+                  srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
+                  loading="lazy"
+                  alt=""
+                />
+              </AspectRatio>
+              <IconButton
+                aria-label="upload new picture"
+                size="sm"
+                variant="outlined"
+                color="neutral"
+                sx={{
+                  bgcolor: "background.body",
+                  position: "absolute",
+                  zIndex: 2,
+                  borderRadius: "50%",
+                  left: 100,
+                  top: 170,
+                  boxShadow: "sm",
+                }}
+              >
+                <EditRoundedIcon />
+              </IconButton>
+            </Stack>
             <Stack spacing={2} sx={{ flexGrow: 1 }}>
               <Stack spacing={1}>
+                <FormLabel>Name</FormLabel>
                 <FormControl
                   sx={{
                     display: {
@@ -140,10 +241,54 @@ export default function MyProfile() {
                     gap: 2,
                   }}
                 >
-                  <Input placeholder="Video Title" />
-                  <Input placeholder="Description" sx={{ flexGrow: 1 }} />
+                  <Input size="sm" placeholder="First name" />
+                  <Input
+                    size="sm"
+                    placeholder="Last name"
+                    sx={{ flexGrow: 1 }}
+                  />
                 </FormControl>
               </Stack>
+              <Stack direction="row" spacing={2}>
+                <FormControl>
+                  <FormLabel>Role</FormLabel>
+                  <Input size="sm" defaultValue="UI Developer" />
+                </FormControl>
+                <FormControl sx={{ flexGrow: 1 }}>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    size="sm"
+                    type="email"
+                    startDecorator={<EmailRoundedIcon />}
+                    placeholder="email"
+                    defaultValue="siriwatk@test.com"
+                    sx={{ flexGrow: 1 }}
+                  />
+                </FormControl>
+              </Stack>
+              <div>
+                <FormControl sx={{ display: { sm: "contents" } }}>
+                  <FormLabel>Timezone</FormLabel>
+                  <Select
+                    size="sm"
+                    startDecorator={<AccessTimeFilledRoundedIcon />}
+                    defaultValue="1"
+                  >
+                    <Option value="1">
+                      Indochina Time (Bangkok){" "}
+                      <Typography textColor="text.tertiary" ml={0.5}>
+                        — GMT+07:00
+                      </Typography>
+                    </Option>
+                    <Option value="2">
+                      Indochina Time (Ho Chi Minh City){" "}
+                      <Typography textColor="text.tertiary" ml={0.5}>
+                        — GMT+07:00
+                      </Typography>
+                    </Option>
+                  </Select>
+                </FormControl>
+              </div>
             </Stack>
           </Stack>
           <Stack
@@ -153,6 +298,18 @@ export default function MyProfile() {
           >
             <Stack direction="row" spacing={2}>
               <Stack direction="column" spacing={1}>
+                <AspectRatio
+                  ratio="1"
+                  maxHeight={108}
+                  sx={{ flex: 1, minWidth: 108, borderRadius: "100%" }}
+                >
+                  <img
+                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
+                    srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
+                    loading="lazy"
+                    alt=""
+                  />
+                </AspectRatio>
                 <IconButton
                   aria-label="upload new picture"
                   size="sm"
@@ -203,36 +360,84 @@ export default function MyProfile() {
                 sx={{ flexGrow: 1 }}
               />
             </FormControl>
+            <div>
+              <FormControl sx={{ display: { sm: "contents" } }}>
+                <FormLabel>Timezone</FormLabel>
+                <Select
+                  size="sm"
+                  startDecorator={<AccessTimeFilledRoundedIcon />}
+                  defaultValue="1"
+                >
+                  <Option value="1">
+                    Indochina Time (Bangkok){" "}
+                    <Typography textColor="text.tertiary" ml={0.5}>
+                      — GMT+07:00
+                    </Typography>
+                  </Option>
+                  <Option value="2">
+                    Indochina Time (Ho Chi Minh City){" "}
+                    <Typography textColor="text.tertiary" ml={0.5}>
+                      — GMT+07:00
+                    </Typography>
+                  </Option>
+                </Select>
+              </FormControl>
+            </div>
           </Stack>
           <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
             <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
-              <Button
-                component="label"
-                role={undefined}
-                tabIndex={-1}
-                variant="outlined"
-                color="neutral"
-                startDecorator={
-                  <SvgIcon>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                      />
-                    </svg>
-                  </SvgIcon>
-                }
-              >
-                Upload a file
-                <VisuallyHiddenInput type="file" />
+              <Button size="sm" variant="outlined" color="neutral">
+                Cancel
               </Button>
+              <Button size="sm" variant="solid">
+                Save
+              </Button>
+            </CardActions>
+          </CardOverflow>
+        </Card>
+        <Card>
+          <Box sx={{ mb: 1 }}>
+            <Typography level="title-md">Bio</Typography>
+            <Typography level="body-sm">
+              Write a short introduction to be displayed on your profile
+            </Typography>
+          </Box>
+          <Divider />
+          <Stack spacing={2} sx={{ my: 1 }}>
+            {/* <EditorToolbar /> */}
+            <Textarea
+              size="sm"
+              minRows={4}
+              sx={{ mt: 1.5 }}
+              defaultValue="I'm a software developer based in Bangkok, Thailand. My goal is to solve UI problems with neat CSS without using too much JavaScript."
+            />
+            <FormHelperText sx={{ mt: 0.75, fontSize: "xs" }}>
+              275 characters left
+            </FormHelperText>
+          </Stack>
+          <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
+            <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
+              <Button size="sm" variant="outlined" color="neutral">
+                Cancel
+              </Button>
+              <Button size="sm" variant="solid">
+                Save
+              </Button>
+            </CardActions>
+          </CardOverflow>
+        </Card>
+        <Card>
+          <Box sx={{ mb: 1 }}>
+            <Typography level="title-md">Portfolio projects</Typography>
+            <Typography level="body-sm">
+              Share a few snippets of your work.
+            </Typography>
+          </Box>
+
+          <Divider />
+          <Stack spacing={2} sx={{ my: 1 }}></Stack>
+          <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
+            <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
               <Button size="sm" variant="outlined" color="neutral">
                 Cancel
               </Button>
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,4 +1,3 @@
-"use client";
 import React from "react";
 import { CssVarsProvider } from "@mui/joy/styles";
 import CssBaseline from "@mui/joy/CssBaseline";
