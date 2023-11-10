a9d1a6ab3794e30587c493fbfcbb01818e6ead15
meet <shaileshpupadhyay1@gmail.com>
2023-11-10 19:08:22 +0530

    get request for videos is added

--- pages/api/video/route.js
+++ pages/api/video/route.js
@@ -2,23 +2,61 @@ const dbConnect = require('../../../../db/mongoose');
 import Video from '../../../db/models/videoModel';
 import { NextResponse } from 'next/server';
 
+export async function GET(req)
+{
+    try {
+        await authController.protect(req);
+
+        const videos = await Video.find({}).populate('uploader');
+
+        return NextResponse.json(
+            {
+                status: 'success',
+                results: videos.length,
+                data: {
+                    videos,
+                    preferredCategories: req.user.preferredCategories,
+                },
+            });
+    } catch (error) {
+        console.log(error);
+        const flag = error instanceof AppError
+        return NextResponse.json(
+            {
+                error: flag ? error.message : 'Internal Server Error'
+            },
+            {
+                status: flag ? error.statusCode : 500
+            },
+        );
+    }
+}
+
 export async function POST(req) {
-    await dbConnect();
-    const data = await req.json();
-    const newVideo = await Videp.create({
-        title: data.title,
-        description: data.description,
-        gDriveID: data.gDriveID,
-        createdAt: data.createdAt,
-        categories: data.categories,
-        uploader: data.uploader,
-    });
-    return NextResponse.json(
-        {
-            message : "New Plan made"
-        },
-        {
-            status : 200
-        }
-    );
+    try {
+        await authController.protect(req);
+
+        const video = Object.assign(req.body, {
+            uploader: req.user._id.toString(),
+        });
+
+        await Video.create(video);
+
+        return NextResponse.json(
+            {
+                status: 'success',
+                message: 'Video added Successfully',
+            });
+    } catch (error) {
+        console.log(error);
+        const flag = error instanceof AppError
+        return NextResponse.json(
+            {
+                error: flag ? error.message : 'Internal Server Error'
+            },
+            {
+                status: flag ? error.statusCode : 500
+            },
+        );
+    }
 }
