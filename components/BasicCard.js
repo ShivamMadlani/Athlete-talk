d817c37308b709f898e44b5be2b88d96a240600e
harshkaranjiya10 <harshkaranjiya10@gmail.com>
2023-11-13 19:13:57 +0530

    replace a tag with iframe

--- components/BasicCard.js
+++ components/BasicCard.js
@@ -11,12 +11,16 @@ export default function BasicCard(props) {
     <div className={styles.aVideo}>
       <Card sx={{ width: 320 }}>
         <div>
-          <Typography level="title-lg" max={props.description}></Typography>
-          <Typography level="body-sm">April 24 to May 02, 2021</Typography>
-        </div>
-        <AspectRatio minHeight="120px" maxHeight="200px">
-          <img src={props.img} srcSet="" loading="lazy" alt="" />
-        </AspectRatio>
+          {/* <Typography level="title-lg" max={props.description}></Typography> */}
+          </div>
+        <iframe
+            src={`https://drive.google.com/file/d/${props.gDriveID}/preview`}
+            width="100%"
+            height="100%"
+            allowFullScreen
+        >
+        </iframe>
+       
         <CardContent orientation="horizontal">
           <div>
             <Typography fontSize="lg" fontWeight="lg">
@@ -24,24 +28,6 @@ export default function BasicCard(props) {
             </Typography>
             <Typography level="body-sm"> {props.trainer} </Typography>
           </div>
-          <a
-            href="https://www.youtube.com/results?search_query=Sport+For+Health%3A+Talking+mental+health"
-            target="_blank" // To open the link in a new tab
-            rel="noopener noreferrer" // Recommended for security
-            max-heigth="36px"
-          >
-            <Button
-              variant="solid"
-              size="md"
-              color="primary"
-              aria-label="Explore Bahamas Islands"
-              sx={{ ml: "auto", alignSelf: "center", fontWeight: 600 }}
-              href="https://www.youtube.com/results?search_query=Sport+For+Health%3A+Talking+mental+health"
-              components="a"
-            >
-              Watch
-            </Button>{" "}
-          </a>
         </CardContent>
       </Card>
     </div>
