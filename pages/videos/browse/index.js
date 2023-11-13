3bf7dbfaed8ae1e78dd28a1a79a0ac88c903d31e
harshkaranjiya10 <harshkaranjiya10@gmail.com>
2023-11-13 19:12:59 +0530

    Fetch videoData from db

--- pages/videos/browse/index.js
+++ pages/videos/browse/index.js
@@ -1,36 +1,55 @@
-import React, { useEffect, useState, useEnhancedEffect } from "react";
-import { CssVarsProvider } from "@mui/joy/styles";
-import CssBaseline from "@mui/joy/CssBaseline";
-import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import Breadcrumbs from "@mui/joy/Breadcrumbs";
-import Link from "@mui/joy/Link";
-import Typography from "@mui/joy/Typography";
-// icons
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
-import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
-import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
-import { nanoid } from "nanoid";
+import React, { useEffect, useState } from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import Link from '@mui/joy/Link';
+import Typography from '@mui/joy/Typography';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import { nanoid } from 'nanoid';
+import Header from '../../../components/header';
+import Sidebar from '../../../components/sidebar';
+import OrderList from '../../../components/OrderList';
+import styles from './index.module.css';
+import BasicCard  from '../../../components/BasicCard';
+const { server } = require('../../../utils/server');
 
-import Header from "../../../components/header";
-import Sidebar from "../../../components/sidebar";
-// import OrderTable from "./components/OrderTable";
-import OrderList from "../../../components/OrderList";
+const Test = ({ videos, preferredCategories }) => {
+  const videosSortedByPreferredCategories = videos.sort((a, b) => {
+    const aCategory = a.categories;
+    const bCategory = b.categories;
+    let presentA = false;
+    let presentB = false;
+    for (let i = 0; i < preferredCategories.length; i++) {
+      if (aCategory.some((category) => category._id === preferredCategories[i])) {
+        presentA = true;
+      }
+      if (bCategory.some((category) => category._id === preferredCategories[i])) {
+        presentB = true;
+      }
+    }
+    if (!presentA && !presentB) {
+      return 0;
+    }
+    if (!presentA) {
+      return 1;
+    }
+    if (!presentB) {
+      return -1;
+    }
+    return 0;
+  });
 
-// Replace useScript with a simple useEffect for now
-import BasicCard from "../../../components/BasicCard";
-import data from "../../../components/dataVideo";
-import styles from "./index.module.css";
-const { server } = require("../../../utils/server");
-
-const test = () => {
-  const VideoCards = data.map((item) => <BasicCard key={item.id} {...item} />);
+  const VideoCards = videosSortedByPreferredCategories.map((video, idx) => (
+    <BasicCard key={idx} {...video} />
+  ));
 
   return (
     <>
       <CssVarsProvider disableTransitionOnChange>
         <CssBaseline />
-        <Box sx={{ display: "flex", minHeight: "100vh" }}>
+        <Box sx={{ display: 'flex', minHeight: '100vh' }}>
           <Header />
           <Sidebar />
           <div className={styles.box}>
@@ -38,41 +57,25 @@ const test = () => {
               component="main"
               className="MainContent"
               sx={{
-                px: {
-                  xs: 2,
-                  md: 6,
-                },
-                pt: {
-                  xs: "calc(12px + var(--Header-height))",
-                  sm: "calc(12px + var(--Header-height))",
-                  md: 3,
-                },
-                pb: {
-                  xs: 2,
-                  sm: 2,
-                  md: 3,
-                },
+                px: { xs: 2, md: 6 },
+                pt: { xs: 'calc(12px + var(--Header-height))', sm: 'calc(12px + var(--Header-height))', md: 3 },
+                pb: { xs: 2, sm: 2, md: 3 },
                 flex: 1,
-                display: "flex",
-                flexDirection: "column",
+                display: 'flex',
+                flexDirection: 'column',
                 minWidth: 0,
-                height: "100dvh",
+                height: '100dvh',
                 gap: 1,
               }}
             >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
                 <Breadcrumbs
                   size="sm"
                   aria-label="breadcrumbs"
                   separator={<ChevronRightRoundedIcon fontSize="sm" />}
                   sx={{ pl: 0 }}
                 >
-                  <Link
-                    underline="none"
-                    color="neutral"
-                    href="#some-link"
-                    aria-label="Home"
-                  >
+                  <Link underline="none" color="neutral" href="#some-link" aria-label="Home">
                     <HomeRoundedIcon />
                   </Link>
                   <Link
@@ -99,8 +102,7 @@ const test = () => {
 export async function getServerSideProps(context) {
   const { req, res } = context;
 
-  if (!req.cookies.jwt)
-    return { redirect: { destination: '/login', permanent: false } };
+  if (!req.cookies.jwt) return { redirect: { destination: '/login', permanent: false } };
 
   try {
     const response = await fetch(`${server}/api/video`, {
@@ -125,7 +127,6 @@ export async function getServerSideProps(context) {
     console.log(err);
     return { redirect: { destination: '/login', permanent: false } };
   }
-  return { redirect: { destination: '/login', permanent: false } };
 }
 
-export default test;
+export default Test;
