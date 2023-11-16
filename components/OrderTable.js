78cebc69528575790fab2cba883ccf34567a7670
harshkaranjiya10 <harshkaranjiya10@gmail.com>
2023-11-16 14:15:55 +0530

    Co-authored-by: Shivam M <ShivamMadlani@users.noreply.github.com>

--- components/OrderTable.js
+++ components/OrderTable.js
@@ -35,7 +35,158 @@ import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
 
-const rows = [];
+let rows = [
+  {
+    id: "INV-1234",
+    date: "Feb 3, 2023",
+    status: "Refunded",
+    customer: {
+      initial: "O",
+      name: "Olivia Ryhe",
+      email: "olivia@email.com",
+    },
+  },
+  {
+    id: "INV-1233",
+    date: "Feb 3, 2023",
+    status: "Paid",
+    customer: {
+      initial: "S",
+      name: "Steve Hampton",
+      email: "steve.hamp@email.com",
+    },
+  },
+  {
+    id: "INV-1232",
+    date: "Feb 3, 2023",
+    status: "Refunded",
+    customer: {
+      initial: "C",
+      name: "Ciaran Murray",
+      email: "ciaran.murray@email.com",
+    },
+  },
+  {
+    id: "INV-1231",
+    date: "Feb 3, 2023",
+    status: "Refunded",
+    customer: {
+      initial: "M",
+      name: "Maria Macdonald",
+      email: "maria.mc@email.com",
+    },
+  },
+  {
+    id: "INV-1230",
+    date: "Feb 3, 2023",
+    status: "Cancelled",
+    customer: {
+      initial: "C",
+      name: "Charles Fulton",
+      email: "fulton@email.com",
+    },
+  },
+  {
+    id: "INV-1229",
+    date: "Feb 3, 2023",
+    status: "Cancelled",
+    customer: {
+      initial: "J",
+      name: "Jay Hooper",
+      email: "hooper@email.com",
+    },
+  },
+  {
+    id: "INV-1228",
+    date: "Feb 3, 2023",
+    status: "Refunded",
+    customer: {
+      initial: "K",
+      name: "Krystal Stevens",
+      email: "k.stevens@email.com",
+    },
+  },
+  {
+    id: "INV-1227",
+    date: "Feb 3, 2023",
+    status: "Paid",
+    customer: {
+      initial: "S",
+      name: "Sachin Flynn",
+      email: "s.flyn@email.com",
+    },
+  },
+  {
+    id: "INV-1226",
+    date: "Feb 3, 2023",
+    status: "Cancelled",
+    customer: {
+      initial: "B",
+      name: "Bradley Rosales",
+      email: "brad123@email.com",
+    },
+  },
+  {
+    id: "INV-1234",
+    date: "Feb 3, 2023",
+    status: "Paid",
+    customer: {
+      initial: "O",
+      name: "Olivia Ryhe",
+      email: "olivia@email.com",
+    },
+  },
+  {
+    id: "INV-1233",
+    date: "Feb 3, 2023",
+    status: "Cancelled",
+    customer: {
+      initial: "S",
+      name: "Steve Hampton",
+      email: "steve.hamp@email.com",
+    },
+  },
+  {
+    id: "INV-1232",
+    date: "Feb 3, 2023",
+    status: "Paid",
+    customer: {
+      initial: "C",
+      name: "Ciaran Murray",
+      email: "ciaran.murray@email.com",
+    },
+  },
+  {
+    id: "INV-1231",
+    date: "Feb 3, 2023",
+    status: "Refunded",
+    customer: {
+      initial: "M",
+      name: "Maria Macdonald",
+      email: "maria.mc@email.com",
+    },
+  },
+  {
+    id: "INV-1230",
+    date: "Feb 3, 2023",
+    status: "Paid",
+    customer: {
+      initial: "C",
+      name: "Charles Fulton",
+      email: "fulton@email.com",
+    },
+  },
+  {
+    id: "INV-1229",
+    date: "Feb 3, 2023",
+    status: "Cancelled",
+    customer: {
+      initial: "J",
+      name: "Jay Hooper",
+      email: "hooper@email.com",
+    },
+  }
+];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -85,27 +236,24 @@ function RowMenu() {
   );
 }
 
-export default function OrderTable() {
-  const [order, setOrder] = useState("desc");
+export default function OrderTable(props) {
+  // rows = props.rows;
+  // const currentDay = props.day;
+  // const setSelectedVideos = props.setVideosSelected;
+
+  const [order, setOrder] = useState("asc");
   const [selected, setSelected] = useState([]);
   const [open, setOpen] = useState(false);
 
+  // React.useEffect(() => {
+  //   setSelected(props.selectedVideos[currentDay]);
+  // }, [props.selectedVideos[currentDay]]);
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
   const renderFilters = () => (
     <React.Fragment>
-      <FormControl size="sm">
-        <FormLabel>Status</FormLabel>
-        <Select
-          size="sm"
-          placeholder="Filter by status"
-          slotProps={{ button: { sx: { whiteSpace: 'nowrap' } } }}
-        >
-          <Option value="paid">Paid</Option>
-          <Option value="pending">Pending</Option>
-          <Option value="refunded">Refunded</Option>
-          <Option value="cancelled">Cancelled</Option>
-        </Select>
-      </FormControl>
-
       <FormControl size="sm">
         <FormLabel>Category</FormLabel>
         <Select size="sm" placeholder="All">
@@ -113,19 +261,6 @@ export default function OrderTable() {
           <Option value="refund">Refund</Option>
           <Option value="purchase">Purchase</Option>
           <Option value="debit">Debit</Option>
-        </Select>
-      </FormControl>
-
-      <FormControl size="sm">
-        <FormLabel>Customer</FormLabel>
-        <Select size="sm" placeholder="All">
-          <Option value="all">All</Option>
-          <Option value="olivia">Olivia Rhye</Option>
-          <Option value="steve">Steve Hampton</Option>
-          <Option value="ciaran">Ciaran Murray</Option>
-          <Option value="marina">Marina Macdonald</Option>
-          <Option value="charles">Charles Fulton</Option>
-          <Option value="jay">Jay Hoper</Option>
         </Select>
       </FormControl>
     </React.Fragment>
@@ -137,8 +272,8 @@ export default function OrderTable() {
         className="SearchAndFilters-mobile"
         sx={{
           display: {
-            xs: 'flex',
-            sm: 'none',
+            xs: "flex",
+            sm: "none",
           },
           my: 1,
           gap: 1,
@@ -165,7 +300,7 @@ export default function OrderTable() {
               Filters
             </Typography>
             <Divider sx={{ my: 2 }} />
-            <Sheet sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Sheet sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
               {renderFilters()}
               <Button color="primary" onClick={() => setOpen(false)}>
                 Submit
@@ -177,25 +312,29 @@ export default function OrderTable() {
       <Box
         className="SearchAndFilters-tabletUp"
         sx={{
-          borderRadius: 'sm',
+          borderRadius: "sm",
           py: 2,
           display: {
-            xs: 'none',
-            sm: 'flex',
+            xs: "none",
+            sm: "flex",
           },
-          flexWrap: 'wrap',
+          flexWrap: "wrap",
           gap: 1.5,
-          '& > *': {
+          "& > *": {
             minWidth: {
-              xs: '120px',
-              md: '160px',
+              xs: "120px",
+              md: "160px",
             },
           },
         }}
       >
         <FormControl sx={{ flex: 1 }} size="sm">
           <FormLabel>Search for order</FormLabel>
-          <Input size="sm" placeholder="Search" startDecorator={<SearchIcon />} />
+          <Input
+            size="sm"
+            placeholder="Search"
+            startDecorator={<SearchIcon />}
+          />
         </FormControl>
         {renderFilters()}
       </Box>
@@ -203,11 +342,11 @@ export default function OrderTable() {
         className="OrderTableContainer"
         variant="outlined"
         sx={{
-          display: { xs: 'none', sm: 'initial' },
-          width: '100%',
-          borderRadius: 'sm',
+          display: { xs: "none", sm: "initial" },
+          width: "100%",
+          borderRadius: "sm",
           flexShrink: 1,
-          overflow: 'auto',
+          overflow: "auto",
           minHeight: 0,
         }}
       >
@@ -216,16 +355,20 @@ export default function OrderTable() {
           stickyHeader
           hoverRow
           sx={{
-            '--TableCell-headBackground': 'var(--joy-palette-background-level1)',
-            '--Table-headerUnderlineThickness': '1px',
-            '--TableRow-hoverBackground': 'var(--joy-palette-background-level1)',
-            '--TableCell-paddingY': '4px',
-            '--TableCell-paddingX': '8px',
+            "--TableCell-headBackground":
+              "var(--joy-palette-background-level1)",
+            "--Table-headerUnderlineThickness": "1px",
+            "--TableRow-hoverBackground":
+              "var(--joy-palette-background-level1)",
+            "--TableCell-paddingY": "4px",
+            "--TableCell-paddingX": "8px",
           }}
         >
           <thead>
             <tr>
-              <th style={{ width: 48, textAlign: 'center', padding: '12px 6px' }}>
+              <th
+                style={{ width: 48, textAlign: "center", padding: "12px 6px" }}
+              >
                 <Checkbox
                   size="sm"
                   indeterminate={
@@ -234,59 +377,58 @@ export default function OrderTable() {
                   checked={selected.length === rows.length}
                   onChange={(event) => {
                     setSelected(
-                      event.target.checked ? rows.map((row) => row.id) : [],
+                      event.target.checked ? rows.map((row) => row.id) : []
                     );
                   }}
                   color={
                     selected.length > 0 || selected.length === rows.length
-                      ? 'primary'
+                      ? "primary"
                       : undefined
                   }
-                  sx={{ verticalAlign: 'text-bottom' }}
+                  sx={{ verticalAlign: "text-bottom" }}
                 />
               </th>
-              <th style={{ width: 120, padding: '12px 6px' }}>
+              <th style={{ width: 120, padding: "12px 6px" }}>
                 <Link
                   underline="none"
                   color="primary"
                   component="button"
-                  onClick={() => setOrder(order === 'asc' ? 'desc' : 'asc')}
+                  onClick={() => setOrder(order === "asc" ? "desc" : "asc")}
                   fontWeight="lg"
                   endDecorator={<ArrowDropDownIcon />}
                   sx={{
-                    '& svg': {
-                      transition: '0.2s',
+                    "& svg": {
+                      transition: "0.2s",
                       transform:
-                        order === 'desc' ? 'rotate(0deg)' : 'rotate(180deg)',
+                        order === "desc" ? "rotate(0deg)" : "rotate(180deg)",
                     },
                   }}
                 >
-                  Invoice
+                  Title
                 </Link>
               </th>
-              <th style={{ width: 140, padding: '12px 6px' }}>Date</th>
-              <th style={{ width: 140, padding: '12px 6px' }}>Status</th>
-              <th style={{ width: 240, padding: '12px 6px' }}>Customer</th>
-              <th style={{ width: 140, padding: '12px 6px' }}> </th>
+              <th style={{ width: 140, padding: "12px 6px" }}>Description</th>
+              <th style={{ width: 140, padding: "12px 6px" }}>Categories</th>
+              <th style={{ width: 140, padding: "12px 6px" }}>Uploader</th>
             </tr>
           </thead>
           <tbody>
-            {stableSort(rows, getComparator(order, 'id')).map((row) => (
+            {stableSort(rows, getComparator(order, "id")).map((row) => (
               <tr key={row.id}>
-                <td style={{ textAlign: 'center', width: 120 }}>
+                <td style={{ textAlign: "center", width: 120 }}>
                   <Checkbox
                     size="sm"
                     checked={selected.includes(row.id)}
-                    color={selected.includes(row.id) ? 'primary' : undefined}
+                    color={selected.includes(row.id) ? "primary" : undefined}
                     onChange={(event) => {
                       setSelected((ids) =>
                         event.target.checked
                           ? ids.concat(row.id)
-                          : ids.filter((itemId) => itemId !== row.id),
+                          : ids.filter((itemId) => itemId !== row.id)
                       );
                     }}
-                    slotProps={{ checkbox: { sx: { textAlign: 'left' } } }}
-                    sx={{ verticalAlign: 'text-bottom' }}
+                    slotProps={{ checkbox: { sx: { textAlign: "left" } } }}
+                    sx={{ verticalAlign: "text-bottom" }}
                   />
                 </td>
                 <td>
@@ -308,9 +450,9 @@ export default function OrderTable() {
                     }
                     color={
                       {
-                        Paid: 'success',
-                        Refunded: 'neutral',
-                        Cancelled: 'danger',
+                        Paid: "success",
+                        Refunded: "neutral",
+                        Cancelled: "danger",
                       }[row.status]
                     }
                   >
@@ -318,20 +460,16 @@ export default function OrderTable() {
                   </Chip>
                 </td>
                 <td>
-                  <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                  <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
                     <Avatar size="sm">{row.customer.initial}</Avatar>
                     <div>
-                      <Typography level="body-xs">{row.customer.name}</Typography>
-                      <Typography level="body-xs">{row.customer.email}</Typography>
+                      <Typography level="body-xs">
+                        {row.customer.name}
+                      </Typography>
+                      <Typography level="body-xs">
+                        {row.customer.email}
+                      </Typography>
                     </div>
-                  </Box>
-                </td>
-                <td>
-                  <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
-                    <Link level="body-xs" component="button">
-                      Download
-                    </Link>
-                    <RowMenu />
                   </Box>
                 </td>
               </tr>
@@ -344,10 +482,10 @@ export default function OrderTable() {
         sx={{
           pt: 2,
           gap: 1,
-          [`& .${iconButtonClasses.root}`]: { borderRadius: '50%' },
+          [`& .${iconButtonClasses.root}`]: { borderRadius: "50%" },
           display: {
-            xs: 'none',
-            md: 'flex',
+            xs: "none",
+            md: "flex",
           },
         }}
       >
@@ -361,11 +499,11 @@ export default function OrderTable() {
         </Button>
 
         <Box sx={{ flex: 1 }} />
-        {['1', '2', '3', '…', '8', '9', '10'].map((page) => (
+        {["1", "2", "3", "…", "8", "9", "10"].map((page) => (
           <IconButton
             key={page}
             size="sm"
-            variant={Number(page) ? 'outlined' : 'plain'}
+            variant={Number(page) ? "outlined" : "plain"}
             color="neutral"
           >
             {page}
