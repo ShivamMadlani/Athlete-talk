0e09c8c0004dbd2539f35f4be4f7918cd589c743
Shivam <shivammadlani5@gmail.com>
2023-11-17 14:52:19 +0530

    revamped contact us page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-'use client'
-import styles from './page.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
-import logo from '../assets/logo.jpg'
-import Typewriter from 'typewriter-effect'
+"use client";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../assets/logo.jpg";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.mainContainer}>
         <div className={styles.header}>
-          <Image src={logo} width={60} className={styles.logo} alt='logo' />
+          <Image src={logo} width={60} className={styles.logo} alt="logo" />
           <h1 className={styles.heading}>Athlete-Talk</h1>
         </div>
         <div className={styles.content}>
@@ -18,24 +18,34 @@ export default function Home() {
             <span className={styles.txt2}>
               <Typewriter
                 options={{
-                  strings: ['Strive, sweat, and succeed.', 'Train tirelessly, triumph triumphantly.', 'Success starts with sacrifice, not shortcuts.', 'Champions chase challenges, conquer courses.'],
+                  strings: [
+                    "Strive, sweat, and succeed.",
+                    "Train tirelessly, triumph triumphantly.",
+                    "Success starts with sacrifice, not shortcuts.",
+                    "Champions chase challenges, conquer courses.",
+                  ],
                   autoStart: true,
                   loop: true,
                 }}
               />
-            </span></h1>
+            </span>
+          </h1>
         </div>
       </div>
       <div className={styles.loginContainer}>
         <h3 className={styles.loginTitle}>Get Started</h3>
         <div className={styles.loginBtns}>
-          <Link href="/login" className={styles.loginBtn}>Login</Link>
-          <Link href="/signup" className={styles.signupBtn}>Sign up</Link>
+          <Link href="/login" className={styles.loginBtn}>
+            Login
+          </Link>
+          <Link href="/signup" className={styles.signupBtn}>
+            Sign up
+          </Link>
         </div>
         <footer className={styles.footer}>
-            &#169; 2023 copyright-<a href="www.google.com">athletetalk.com</a>
+          <Link href="/contactus">&#169; 2023 copyright athletetalk.com</Link>
         </footer>
       </div>
     </main>
-  )
+  );
 }
