00bfe06dbf3799b59f252910728ff725a98507d5
Shivam <shivammadlani5@gmail.com>
2023-11-24 21:09:59 +0530

    updated foldernames

---
+++ pages/forgetPassword/index.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Box, Typography, Input, Button, FormLabel } from "@mui/joy";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = {
+      email: email,
+    };
+
+    const response = await fetch(`/api/forgetPassword`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    const responseData = await response.json();
+
+    if (response.ok) {
+      return alert("reset link sent");
+    }
+
+    return alert("Something went wrong");
+  };
+
+  const pageContainerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+    backgroundColor: "#f9f9f9",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const formContainerStyle = {
+    width: "500px",
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    padding: "20px",
+    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+  };
+
+  return (
+    <div style={pageContainerStyle}>
+      <Box
+        sx={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={formContainerStyle}>
+          <Typography
+            level="title-lg"
+            color="primary"
+            textAlign="center"
+            mb="20px"
+          >
+            Forgot Password
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <FormLabel>Email</FormLabel>
+            <Input
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={handleChange}
+              sx={{ mb: "20px" }}
+              required
+            />
+            <Button type="submit" sx={{ width: "100%" }}>
+              Reset Password
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default ForgotPassword;
