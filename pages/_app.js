47768006e961a46c2f0aa719c5098db8c0520a2d
Shivam <shivammadlani5@gmail.com>
2023-11-26 01:55:24 +0530

    added alt tag and logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import { AuthContextProvider } from '../authCtx'
-import './globals.css'
+import Head from "next/head";
+import { AuthContextProvider } from "../authCtx";
+import "./globals.css";
 
 export default function App({ Component, pageProps }) {
-    return (
-        <AuthContextProvider>
-            <Component {...pageProps} />
-        </AuthContextProvider>
-    )
+  return (
+    <AuthContextProvider>
+      <Head>
+        <title>Athlete Talk</title>
+        <link rel="icon" type="image/x-icon" href="/logo.jpg" />
+      </Head>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
