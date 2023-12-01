869781aa48d2d11e1135a2c4aaeb803a198ce5a7
harshkaranjiya10 <harshkaranjiya10@gmail.com>
2023-12-01 21:56:34 +0530

    edited profile page

--- components/MyProfile.js
+++ components/MyProfile.js
@@ -179,16 +179,16 @@ export default function MyProfile() {
         }}
       >
         <Card>
-          <Box sx={{ mb: 1 }}>
+          <Box>
             <Typography level="title-md">Personal info</Typography>
-            <Typography level="body-sm">
+            {/* <Typography level="body-sm">
               Customize how your profile information will apper to the networks.
-            </Typography>
+            </Typography> */}
           </Box>
           <Divider />
           <Stack
             direction="row"
-            spacing={3}
+            spacing={5}
             sx={{ display: { xs: "none", md: "flex" }, my: 1 }}
           >
             <Stack direction="column" spacing={1}>
@@ -204,65 +204,17 @@ export default function MyProfile() {
                   alt=""
                 />
               </AspectRatio>
-              <IconButton
-                aria-label="upload new picture"
-                size="sm"
-                variant="outlined"
-                color="neutral"
-                sx={{
-                  bgcolor: "background.body",
-                  position: "absolute",
-                  zIndex: 2,
-                  borderRadius: "50%",
-                  left: 100,
-                  top: 170,
-                  boxShadow: "sm",
-                }}
-              >
-                <EditRoundedIcon />
-              </IconButton>
             </Stack>
-            <Stack spacing={2} sx={{ flexGrow: 1 }}>
-              <Stack spacing={1}>
-                <FormLabel>Name</FormLabel>
-                <FormControl
-                  sx={{
-                    display: {
-                      sm: "flex-column",
-                      md: "flex-row",
-                    },
-                    gap: 2,
-                  }}
-                >
-                  <Input size="sm" placeholder={profile.name} />
-                  <Input
-                    size="sm"
-                    placeholder="Last name"
-                    sx={{ flexGrow: 1 }}
-                  />
-                </FormControl>
-              </Stack>
-              <Stack direction="row" spacing={2}>
-                <FormControl>
-                  <FormLabel>Role</FormLabel>
-                  <Input size="sm" placeholder={profile.role} />
-                </FormControl>
-                <FormControl sx={{ flexGrow: 1 }}>
-                  <FormLabel>Email</FormLabel>
-                  <Input
-                    size="sm"
-                    type="email"
-                    startDecorator={<EmailRoundedIcon />}
-                    placeholder={profile.email}
-                    sx={{ flexGrow: 1 }}
-                  />
-                </FormControl>
-              </Stack>
+
+            <Stack spacing={2}>
+              <Typography>{profile.name}</Typography>
+              <Typography>{profile.role}</Typography>
+              <Typography>{profile.email}</Typography>
             </Stack>
           </Stack>
           <Stack
             direction="column"
-            spacing={2}
+            spacing={5}
             sx={{ display: { xs: "flex", md: "none" }, my: 1 }}
           >
             <Stack direction="row" spacing={2}>
@@ -279,69 +231,16 @@ export default function MyProfile() {
                     alt=""
                   />
                 </AspectRatio>
-                <IconButton
-                  aria-label="upload new picture"
-                  size="sm"
-                  variant="outlined"
-                  color="neutral"
-                  sx={{
-                    bgcolor: "background.body",
-                    position: "absolute",
-                    zIndex: 2,
-                    borderRadius: "50%",
-                    left: 85,
-                    top: 180,
-                    boxShadow: "sm",
-                  }}
-                >
-                  <EditRoundedIcon />
-                </IconButton>
               </Stack>
-              <Stack spacing={1} sx={{ flexGrow: 1 }}>
-                <FormLabel>Name</FormLabel>
-                <FormControl
-                  sx={{
-                    display: {
-                      sm: "flex-column",
-                      md: "flex-row",
-                    },
-                    gap: 2,
-                  }}
-                >
-                  <Input size="sm" placeholder="First name" />
-                  <Input size="sm" placeholder="Last name" />
-                </FormControl>
+              <Stack spacing={2} sx={{ flexGrow: 1 }}>
+                <Typography>{profile.name}</Typography>
+                <Typography>{profile.role}</Typography>
+                <Typography>{profile.email}</Typography>
               </Stack>
             </Stack>
-
-            <FormControl>
-              <FormLabel>Role</FormLabel>
-              <Input size="sm" defaultValue="Athlete" />
-            </FormControl>
-            <FormControl sx={{ flexGrow: 1 }}>
-              <FormLabel>Email</FormLabel>
-              <Input
-                size="sm"
-                type="email"
-                startDecorator={<EmailRoundedIcon />}
-                placeholder="email"
-                defaultValue="siriwatk@test.com"
-                sx={{ flexGrow: 1 }}
-              />
-            </FormControl>
           </Stack>
-          <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
-            <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
-              <Button size="sm" variant="outlined" color="neutral">
-                Cancel
-              </Button>
-              <Button size="sm" variant="solid">
-                Save
-              </Button>
-            </CardActions>
-          </CardOverflow>
         </Card>
-        <Card>
+        {/* <Card>
           <Box sx={{ mb: 1 }}>
             <Typography level="title-md">Bio</Typography>
             <Typography level="body-sm">
@@ -365,7 +264,7 @@ export default function MyProfile() {
               </Button>
             </CardActions>
           </CardOverflow>
-        </Card>
+        </Card> */}
       </Stack>
     </Box>
   );
