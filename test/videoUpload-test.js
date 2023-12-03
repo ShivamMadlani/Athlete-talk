9fea95a8c6e59945076eba8b5237dba601307eea
meet <shaileshpupadhyay1@gmail.com>
2023-12-03 19:49:48 +0530

    video-Upload test added

---
+++ test/videoUpload-test.js
@@ -0,0 +1,40 @@
+var assert = require("assert");
+const request = require("supertest");
+
+let token;
+before(async () => {
+  const body = {
+    email: "testing2@gmail.com",
+    password: "testing2",
+  };
+  const res = await fetch("http://localhost:3000/api/users/login", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  const resd = await res.json();
+  token = resd.token;
+  console.log(token);
+});
+
+
+
+
+describe('Upload Video Testing', () => {
+  const host = "http://localhost:3000";
+  const path = "/api/video/upload";
+  it('should upload a file', (done) => {
+    request(host)
+      .post(path)
+      .field('title', 'test1234')
+      .field('description', 'hello i am here to perform testing')
+      .attach('file', "D://sowftware//Athlete-talk//testvideo.mp4") // Adjust the file path accordingly
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        assert.strictEqual(res.body.status, 'success');
+        done();
+      });
+  }).timeout(60000);
+});
